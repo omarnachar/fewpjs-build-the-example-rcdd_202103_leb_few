c6dd84d95d37085870d604a8bdeaff44127dfe02
Omar Nachar <omarnachar2@gmail.com>
2021-05-20 13:05:28 +0000

    Done.

--- main.js
+++ main.js
@@ -4,7 +4,25 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const modal = document.getElementById('modal')
+const likeButtons = document.getElementsByClassName('like-glyph')
 
+modal.className = "hidden"
+
+for (const heart of likeButtons) {
+  heart.addEventListener('click', () => {
+    mimicServerCall()
+    .then(()=> {
+      heart.className = "activated-heart"
+    })
+    .catch(() => {
+      modal.style.visibility = "visible"
+    })
+  })
+}
+document.addEventListener('DOMContentLoaded', function() {
+
+})
 
 
 //------------------------------------------------------------------------------
